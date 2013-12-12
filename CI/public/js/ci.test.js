f7de6c562767e453f17b3c9b4d283a7a7d0f2831
Lucas Tettamanti <ltettamanti@despegar.com>
2013-12-12 00:16:51 -0300

    include tests

---
+++ CI/public/js/ci.test.js
@@ -0,0 +1,41 @@
+ci.test = (function() {
+  'use strict';
+
+  // declarations
+  var configMap = {
+      main_html: String()
+        + '<div id="test-test"></div>'
+    }, stateMap = {
+      $container: undefined
+    }, jqueryMap = {},
+    setJqueryMap, handler, initModule, test;
+
+  // utils methods
+  setJqueryMap = function() {
+    var $container = stateMap.$container;
+    jqueryMap = {
+      $container: stateMap.$container,
+      $test: $container.find('#test-test')
+    }
+  };
+
+  // handlers
+  handler = function(arg) {
+    initModule($("#shell-header"));
+    console.log(arg);
+    return false;
+  };
+
+  // dom methods
+
+  // public functions
+  initModule = function($container) {
+    stateMap.$container = $container;
+    $container.html(configMap.main_html);
+    setJqueryMap();
+
+    // bind to events
+    //$(window).bind('hashchange', onHashChange).trigger('hashchange');
+  };
+  return {handler:handler}
+}());
