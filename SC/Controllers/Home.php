796021a9f66dba65219769dad0d150e64a7dd547
Lucas Tettamanti <ltettamanti@despegar.com>
2014-01-01 23:35:54 -0300

    Test dependencies

--- SC/Controllers/Home.php
+++ SC/Controllers/Home.php
@@ -9,4 +9,14 @@ class Controllers_Home extends Controller {
         echo "<pre>";
         var_dump($args);
     }
+
+    public function test_dependencies() {
+        $d = new Dependencies();
+        $d->add_data("segundo", "segundo", ["primero"]);
+        $d->add_data("quinto", "quinto", ["primero", "cuarto"]);
+        $d->add_data("primero", "primero");
+        $d->add_data("cuarto", "cuarto", ["tercero"]);
+        $d->add_data("tercero", "tercero", ["segundo"]);
+        echo "<pre>";var_dump($d->get_data());
+    }
 }
