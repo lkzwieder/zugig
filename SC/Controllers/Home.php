fb30b931e912957fb2239c5b0597d89d91d16f30
Lucas Tettamanti <ltettamanti@despegar.com>
2014-01-03 16:10:27 -0300

    more glue

--- SC/Controllers/Home.php
+++ SC/Controllers/Home.php
@@ -1,22 +1,15 @@
 <?php
 class Controllers_Home extends Controller {
     public function index() {
-        $this->set_vars(['title' => 'Scholar beta']);
+        $this->set_vars([
+            'title' => 'Scholar beta',
+            'glue_js' => GlueJS::get_instance(),
+        ]);
         $this->render();
     }
 
     public function home($args) {
         echo "<pre>";
         var_dump($args);
-    }
-
-    public function test_dependencies() {
-        $d = new Dependencies();
-        $d->add_data("segundo", "segundo", ["primero"]);
-        $d->add_data("quinto", "quinto", ["primero", "cuarto"]);
-        $d->add_data("primero", "primero");
-        $d->add_data("cuarto", "cuarto", ["tercero"]);
-        $d->add_data("tercero", "tercero", ["segundo"]);
-        echo "<pre>";var_dump($d->get_data());
     }
 }
